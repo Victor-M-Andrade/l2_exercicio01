76f745fa078e4d00733e3c444a6adec5c9224a1d
Victor Andrade <victorma2000@live.com>
2021-03-15 20:59:19 -0300

    Mapa base criado

--- src/main/java/l2_exercicio01/Main.java
+++ src/main/java/l2_exercicio01/Main.java
@@ -1,10 +1,22 @@
 package l2_exercicio01;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
 
+	}
+
+	
+	private Map<Integer, String> mapaDeCandidatos = new HashMap<Integer, String>();
+	
+	private void start() {
+		
+		
 	}
 
 }
