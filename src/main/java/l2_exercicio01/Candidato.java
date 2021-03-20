22dd99736acd919d8aa3d48f9170749ce54708b4
Victor Andrade <victorma2000@live.com>
2021-03-20 13:07:49 -0300

    não consegui fazer o registrar voto

---
+++ src/main/java/l2_exercicio01/Candidato.java
@@ -0,0 +1,22 @@
+package l2_exercicio01;
+
+public class Candidato {
+
+	public Candidato(int id, String nome) {
+
+		this.id = id;
+		this.nome = nome;
+	}
+
+	private int id;
+	private String nome;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+}
--- src/main/java/l2_exercicio01/Main.java
+++ src/main/java/l2_exercicio01/Main.java
@@ -2,6 +2,7 @@ package l2_exercicio01;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,12 +12,67 @@ public class Main {
 
 	}
 
-	
-	private Map<Integer, String> mapaDeCandidatos = new HashMap<Integer, String>();
-	
+	private Map<Integer, Candidato> mapaDeCandidatos = new HashMap<Integer, Candidato>();
+
 	private void start() {
-		
-		
+		inicializarMapaDeCandidatos();
+
+		while (true) {
+
+			int voto = obterVoto();
+			if (voto == 0) {
+				break;
+			}
+
+			System.out.println("Número do voto: " + voto);
+		}
+
+		System.out.println("Fim da Aplicação");
+
 	}
 
+	private void inicializarMapaDeCandidatos() {
+
+		Candidato joao = new Candidato(1, "João Gumercino");
+		mapaDeCandidatos.put(joao.getId(), joao);
+
+		Candidato maria = new Candidato(2, "Maria Madalena");
+		mapaDeCandidatos.put(maria.getId(), maria);
+
+		Candidato jose = new Candidato(3, "José Jounes");
+		mapaDeCandidatos.put(jose.getId(), jose);
+
+		Candidato antonio = new Candidato(4, "Antônio Maomé");
+		mapaDeCandidatos.put(antonio.getId(), antonio);
+
+		Candidato tibursinho = new Candidato(5, "Tibursinho VidaLoka");
+		mapaDeCandidatos.put(tibursinho.getId(), tibursinho);
+
+	}
+
+	private int obterVoto() {
+
+		System.out.println("Digite seu voto: ");
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+
+			return scanner.nextInt();
+		} catch (Exception e) {
+
+			scanner.next();
+
+			System.out.println("Valor inválido");
+			return -1;
+		}
+
+	}
+
+	private void registrarVoto(int voto) {
+		/*
+		 * NOME_MAPA.containsKey(CHAVE_EM_QUESTAO) NOME_MAPA.get(CHAVE_EM_QUESTAO)
+		 * não faço a minima ideia de como fazer
+		 */
+
+	}
 }
